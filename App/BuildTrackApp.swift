ae9504a899666d506e4f90b87ed6e5f724cec135
BuildTrack CI <buildtrack@stancainvest.ro>
2026-05-08 23:20:31 +0000

    Fix BuildTrackApp.swift: use SwiftDataStack.previewContainer
    - Simplify app init to avoid @MainActor isolation issues
    - Use shared previewContainer() for ModelContainer

--- App/BuildTrackApp.swift
+++ App/BuildTrackApp.swift
@@ -4,29 +4,12 @@ import SwiftData
 @main
 struct BuildTrackApp: App {
     @State private var authManager = AuthManager()
-    let modelContainer: ModelContainer
-    
-    init() {
-        let schema = Schema([
-            Project.self,
-            TaskItem.self,
-            Incident.self,
-            Inspection.self,
-            Worker.self
-        ])
-        let config = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-        do {
-            self.modelContainer = try ModelContainer(for: schema, configurations: config)
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }
     
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environment(authManager)
         }
-        .modelContainer(modelContainer)
+        .modelContainer(SwiftDataStack.previewContainer())
     }
 }
